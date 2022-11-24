7dc2b5f290635fc9b335b2422d421b621db6ea41
Christopher Nielson <cnielson3@gmail.com>
2022-11-24 14:10:53 -0500

    Updated save method so that results are written to .csv file as they're done rather than pushing results to DataFrame before saving. Should be more streamlined. Added dialogs for save file and number of threads. Added documentation. Removed unnecessary comments and code.

--- main.py
+++ main.py
@@ -1,20 +1,60 @@
 import concurrent.futures
+import csv
 import functools
 import itertools
 import math
+import threading
 from os import listdir, path
-from tkinter import filedialog
+from tkinter import filedialog, simpledialog
 
+import pandas
 import pandas as pd
 from molmass import ELEMENTS
 
 
-def calc_com(m1, c1, m2, c2):
+def calc_com(m1: float, c1: pandas.Series, m2: float, c2: pandas.Series) -> pandas.Series:
+	"""
+	Calculates the center of mass between the given masses and their coordinate tuples.
+
+	:param m1: Mass 1
+	:type m1:
+	:param c1: Coordinates of m1
+	:type c1:
+	:param m2: Mass 2
+	:type m2:
+	:param c2: Coordinates of m2
+	:type c2:
+	:return: Center of mass coordinates
+	:rtype: tuple(float)
+	"""
 	return (m1 * c1 + m2 * c2) / (m1 + m2)
 
 
-def process_xyz(train_df, xyz_file, file_num, total_files):
-	print(f'Processing file {file_num} / {total_files}:\n\t{xyz_file}')
+def process_xyz(
+	train_df: pandas.DataFrame, csv_out: csv.writer, lock: threading.Lock, xyz_file: str, file_num: int = None, total_files: int = None
+) -> None:
+	"""
+	Processes one .xyz file; calculates centers of mass of atom combinations and finds nearest neighbors. Writes result using csv_out.
+
+	:param train_df: DataFrame containing all training points
+	:type train_df:
+	:param csv_out: csv.writer object that is already configured
+	:type csv_out: csv.writer
+	:param lock: Lock object
+	:type lock:
+	:param xyz_file: .xyz file to process
+	:type xyz_file:
+	:param file_num: current number of file; for console writing purposes only
+	:type file_num:
+	:param total_files: total number of files; for console writing purposes only
+	:type total_files:
+	:return: None
+	:rtype:
+	"""
+	if file_num is not None and total_files is not None:
+		lock.acquire()
+		print(f'Processing file {file_num} / {total_files}...\n\t{xyz_file}')
+		lock.release()
 
 	# Molecule identifier
 	mol_id = path.splitext(path.basename(xyz_file))[0]
@@ -25,100 +65,91 @@ def process_xyz(train_df, xyz_file, file_num, total_files):
 	# Rows that match the current .xyz file molecule
 	matching_atom_pairs = train_df[train_df['molecule_name'] == mol_id]
 
-	result = [train_df]
-
 	# Iterate through atomic pairs from matching_atom_pairs
 	for (_, row) in matching_atom_pairs.iterrows():
 		# Row of corresponding atoms in pair
-		atom_1_index = row['atom_index_0']
-		atom_2_index = row['atom_index_1']
+		atom_0_index = row['atom_index_0']
+		atom_1_index = row['atom_index_1']
 
 		# Sets to atomic symbol
+		atom_0_type = current_xyz.iloc[atom_0_index, 0]
 		atom_1_type = current_xyz.iloc[atom_1_index, 0]
-		atom_2_type = current_xyz.iloc[atom_2_index, 0]
 
 		# Pandas Series; index 1 is x, 2 is y, 3 is z coordinate
+		atom_0_coords = current_xyz.iloc[atom_0_index, 1:4]
 		atom_1_coords = current_xyz.iloc[atom_1_index, 1:4]
-		atom_2_coords = current_xyz.iloc[atom_2_index, 1:4]
 
 		# Calculate Center of Mass for the atom pair
-		com = calc_com(ELEMENTS[atom_1_type].mass, atom_1_coords, ELEMENTS[atom_2_type].mass, atom_2_coords)
+		com = calc_com(ELEMENTS[atom_0_type].mass, atom_0_coords, ELEMENTS[atom_1_type].mass, atom_1_coords)
 
 		# Dictionary of atom index and (mass-weighted) distance to atomic pair COM
 		distances = {}
 
 		# List of atom indices excluding the current atom pair
 		index_list = filter(
-			lambda i: i != atom_1_index and i != atom_2_index, range(0, current_xyz.shape[0])
+			lambda i: i != atom_0_index and i != atom_1_index, range(0, current_xyz.shape[0])
 		)
 
 		# Iterate through index_list and calculate (mass-weighted) COM between atom and current pair COM
 		for atom in current_xyz.iloc[index_list].iterrows():
 			# Center of mass between atomic pair COM and atom
-			pair_com = calc_com((ELEMENTS[atom_1_type].mass + ELEMENTS[atom_2_type].mass) / 2, com, ELEMENTS[atom[1][0]].mass, atom[1][1:4])
+			pair_com = calc_com((ELEMENTS[atom_0_type].mass + ELEMENTS[atom_1_type].mass) / 2, com, ELEMENTS[atom[1][0]].mass, atom[1][1:4])
 			distances[atom[0]] = math.sqrt(functools.reduce(lambda x, y: x + y, (com.array - pair_com) ** 2))
 
 		# Sort by distance and calculate closest 2
 		sorted_distances = {k: v for k, v in sorted(distances.items(), key=lambda item: item[1])}
 		closest_2 = tuple(itertools.islice(sorted_distances.items(), 2))
 
-		result.append(
-			{
-				'index': row['id'],
-				'atom_type_0': atom_1_type,
-				'atom_type_1': atom_2_type,
-				'n1_mol': current_xyz.iloc[closest_2[0][0]][0],
-				'n1_dist': closest_2[0][1],
-				'n2_mol': current_xyz.iloc[closest_2[1][0]][0] if len(closest_2) > 1 else '',
-				'n2_dist': closest_2[1][1] if len(closest_2) > 1 else 0
-			}
+		# Write results to output .csv file
+		lock.acquire()
+		print(f'Writing row {row["id"]}...')
+
+		# Write result to new row
+		csv_out.writerow(
+			[row['id'], mol_id, atom_0_index, atom_0_type, atom_1_index, atom_1_type, row['type'], row['scalar_coupling_constant'],
+				current_xyz.iloc[closest_2[0][0]][0], closest_2[0][1], current_xyz.iloc[closest_2[1][0]][0] if len(closest_2) > 1 else '',
+				closest_2[1][1] if len(closest_2) > 1 else 0]
 		)
-
-	return result
-
-
-def update_df(future: concurrent.futures.Future):
-	train_df = future.result()[0]
-
-	for result in future.result()[1:]:
-		train_df.loc[result['index'], 'n1_mol'] = result['n1_mol']
-		train_df.loc[result['index'], 'n1_dist'] = result['n1_dist']
-
-		train_df.loc[result['index'], 'n2_mol'] = result['n2_mol']
-		train_df.loc[result['index'], 'n2_dist'] = result['n2_dist']
-
-		train_df.loc[result['index'], 'atom_type_0'] = result['atom_type_0']
-		train_df.loc[result['index'], 'atom_type_1'] = result['atom_type_1']
+		lock.release()
 
 
-def main():
+def main() -> None:
+	"""
+	Main entrypoint to program. Sets up the input and output files and spins off threads to handle each .xyz file.
+	:return: None
+	:rtype: None
+	"""
+	# Open dialogs to select input and output files, and number of threads to run on
 	train_file = filedialog.askopenfilename(title='Select Training File', initialdir='./')
 	structures_dir = filedialog.askdirectory(title='Select Structures Directory', initialdir='./')
+	output_file = filedialog.asksaveasfilename(title='Select Save File', initialdir='./', filetypes=[('CSV', '*.csv')])
+	num_threads = simpledialog.askinteger(title='Number of Threads', prompt='How many threads do you want to use?')
 
-	xyz_list = listdir(structures_dir)  # List of .xyz files
+	xyz_list = listdir(structures_dir)
 	total_files = len(xyz_list)
 	num_files = iter(range(1, total_files + 1))
 
 	train_df = pd.read_csv(train_file)
 
-	train_df['atom_type_0'] = ''
-	train_df['atom_type_1'] = ''
-	train_df['n1_mol'] = ''
-	train_df['n1_dist'] = 0
-	train_df['n2_mol'] = ''
-	train_df['n2_dist'] = 0
+	lock = threading.Lock()
 
-	with concurrent.futures.ThreadPoolExecutor(12) as executor:
-		futures = [executor.submit(
-			process_xyz, train_df, structures_dir + '/' + file, next(num_files), total_files
-		) for file in xyz_list]
-		for future in futures:
-			future.add_done_callback(update_df)
-		concurrent.futures.wait(futures)  # executor.shutdown()
+	with open(output_file, 'w', newline='') as csv_file:
+		csv_out = csv.writer(csv_file)
+		# Write header
+		csv_out.writerow(
+			['id', 'molecule_name', 'atom_index_0', 'atom_type_0', 'atom_index_1', 'atom_type_1', 'type', 'scalar_coupling_constant', 'n1_type',
+				'n1_dist', 'n2_type', 'n2_dist']
+		)
 
-	print('Saving...')
-	train_df.to_csv('./data/train_w_knn.csv')
-	print('Finished!')
+		# Spin off threads
+		with concurrent.futures.ThreadPoolExecutor(num_threads) as executor:
+			futures = [executor.submit(
+				process_xyz, train_df, csv_out, lock, structures_dir + '/' + file, next(num_files), total_files
+			) for file in xyz_list]
+			# Wait for all .xyz files to be processed
+			concurrent.futures.wait(futures)
+
+	print('\nFinished!')
 
 
 if __name__ == "__main__":
