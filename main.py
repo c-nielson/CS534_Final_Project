18cf42faabba0872aa4424e53d7319cf8f20db5a
Christopher Nielson <cnielson3@gmail.com>
2022-11-28 09:54:55 -0500

    Updated output to include distance of initial pair, atomic numbers. Changed scaling on neighbor COM to artificially make heavier atoms appear closer rather than further.

--- main.py
+++ main.py
@@ -11,6 +11,20 @@ import pandas as pd
 from molmass import ELEMENTS
 
 
+def get_dist(a, b) -> float:
+	"""
+	Returns distance between two sets of coordinates.
+
+	:param a: First coordinate
+	:type a: Array-like
+	:param b: Second coordinate
+	:type b: Array-like
+	:return: Distance between a and b
+	:rtype: float
+	"""
+	return math.sqrt(functools.reduce(lambda x, y: x + y, (a - b) ** 2))
+
+
 def calc_com(m1: float, c1: pandas.Series, m2: float, c2: pandas.Series) -> pandas.Series:
 	"""
 	Calculates the center of mass between the given masses and their coordinate tuples.
@@ -84,20 +98,27 @@ def process_xyz(
 
 		# Iterate through index_list and calculate (mass-weighted) COM between atom and current pair COM
 		for atom in current_xyz.iloc[index_list].iterrows():
-			# Center of mass between atomic pair COM and atom
-			pair_com = calc_com((ELEMENTS[atom_0_type].mass + ELEMENTS[atom_1_type].mass) / 2, com, ELEMENTS[atom[1][0]].mass, atom[1][1:4])
-			distances[atom[0]] = math.sqrt(functools.reduce(lambda x, y: x + y, (com.array - pair_com) ** 2))
+			# Center of mass between atomic pair COM and atom, scaled inversely to neighbour mass (i.e. heavier atoms appear closer than smaller
+			# atoms)
+			pair_com = calc_com((ELEMENTS[atom_0_type].mass + ELEMENTS[atom_1_type].mass) / 2, com, ELEMENTS[atom[1][0]].mass, atom[1][1:4]) * (
+					(ELEMENTS[atom_0_type].mass + ELEMENTS[atom_1_type].mass) / 2) / ELEMENTS[atom[1][0]].mass
+			distances[atom[0]] = get_dist(com.array, pair_com)
 
 		# Sort by distance and calculate closest 2
 		sorted_distances = {k: v for k, v in sorted(distances.items(), key=lambda item: item[1])}
 		closest_2 = tuple(itertools.islice(sorted_distances.items(), 2))
 
+		# Get atom types of 2 closest neighbours
+		n1_type = current_xyz.iloc[closest_2[0][0]][0]
+		n2_type = current_xyz.iloc[closest_2[1][0]][0] if len(closest_2) > 1 else ''
+
 		results.append(
-			[row['id'], mol_id, atom_0_index, atom_0_type, atom_1_index, atom_1_type, row['type'], row['scalar_coupling_constant'],
-				current_xyz.iloc[closest_2[0][0]][0], closest_2[0][1], current_xyz.iloc[closest_2[1][0]][0] if len(closest_2) > 1 else '',
-				closest_2[1][1] if len(closest_2) > 1 else 0]
+			[row['id'], mol_id, atom_0_index, atom_0_type, ELEMENTS[atom_0_type].number, atom_1_index, atom_1_type, ELEMENTS[atom_1_type].number,
+				get_dist(atom_0_coords.array, atom_1_coords.array), row['type'], row['scalar_coupling_constant'], n1_type, ELEMENTS[n1_type].number,
+				closest_2[0][1], n2_type, '' if n2_type == '' else ELEMENTS[n2_type].number, closest_2[1][1] if len(closest_2) > 1 else 0]
 		)
 
+	# Just for logging to track progress
 	if file_counter is not None and total_files is not None:
 		print(f'\tFinished {next(file_counter)} / {total_files}:\n\t\t{xyz_file}')
 
@@ -140,10 +161,11 @@ def main() -> None:
 		csv_out = csv.writer(csv_file)
 		# Write header
 		csv_out.writerow(
-			['id', 'molecule_name', 'atom_index_0', 'atom_type_0', 'atom_index_1', 'atom_type_1', 'type', 'scalar_coupling_constant', 'n1_type',
-				'n1_dist', 'n2_type', 'n2_dist']
+			['id', 'molecule_name', 'atom_index_0', 'atom_type_0', 'atomic_number_0', 'atom_index_1', 'atom_type_1', 'atomic_number_1', 'pair_dist',
+				'type', 'scalar_coupling_constant', 'n1_type', 'n1_number', 'n1_dist', 'n2_type', 'n2_number', 'n2_dist']
 		)
 
+		# Write results of all Futures
 		for future in futures:
 			print(f'\t{next(futures_num)} / {futures_total}')
 			for result in future.result():
@@ -152,6 +174,7 @@ def main() -> None:
 	print('\nFinished!')
 
 
+# Entry point
 if __name__ == "__main__":
 	import sys
 
