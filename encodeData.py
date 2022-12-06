ecc9999cfd7afc81eb44c150a4b10259fe6bf17f
Christopher Nielson <cnielson3@gmail.com>
2022-12-06 13:06:53 -0500

    Changed encoding to iterate through columns rather than load all at once; helps with memory.

--- encodeData.py
+++ encodeData.py
@@ -1,53 +1,59 @@
 from tkinter import filedialog
 
 import pandas as pd
-
-
-# from sklearn import preprocessing
+from sklearn import preprocessing
 
 
 def main():
-	data_in = filedialog.askopenfilename(title='Select File for Encoding', initialdir='./')
-	data_out = filedialog.asksaveasfilename(title='Select File to Save Results', initialdir='./', filetypes=[('CSV', '*.csv')])
+	data_in = filedialog.askopenfilename(title='Select File for Encoding', initialdir='./data/')
+	data_out = filedialog.asksaveasfilename(title='Select File to Save Results', initialdir='./data/', filetypes=[('CSV', '*.csv')])
 
-	df = pd.read_csv(data_in)
+	cols_to_encode = [
+		'atom_type_0',
+		'atom_type_1',
+		'type',
+		'n1_type',
+		'n2_type',
+		'n3_type',
+		'n4_type',
+		'n5_type',
+		'n6_type',
+		'n7_type',
+		'n8_type',
+		'n9_type',
+		'n10_type'
+	]
 
-	dummies = pd.get_dummies(
-		df[[
-			'atom_type_0',
-			'atom_type_1',
-			'type',
-			'n1_type',
-			'n2_type',
-			'n3_type',
-			'n4_type',
-			'n5_type',
-			'n6_type',
-			'n7_type',
-			'n8_type',
-			'n9_type',
-			'n10_type'
-		]],
-		dummy_na=True
+	df = pd.read_csv(
+		data_in,
+		usecols=cols_to_encode
 	)
 
-	df_encoded = df[[
-		'pair_dist',
-		'n1_dist',
-		'n2_dist',
-		'n3_dist',
-		'n4_dist',
-		'n5_dist',
-		'n6_dist',
-		'n7_dist',
-		'n8_dist',
-		'n9_dist',
-		'n10_dist',
-		'scalar_coupling_constant'
-	]].join(dummies)
+	df_encoded = pd.read_csv(
+		data_in,
+		usecols=[
+			'pair_dist',
+			'n1_dist',
+			'n2_dist',
+			'n3_dist',
+			'n4_dist',
+			'n5_dist',
+			'n6_dist',
+			'n7_dist',
+			'n8_dist',
+			'n9_dist',
+			'n10_dist',
+			'scalar_coupling_constant'
+		]
+	)
 
-	# max_abs_scaler = preprocessing.MaxAbsScaler()
-	# df_encoded[['scalar_coupling_constant']] = max_abs_scaler.fit_transform(df_encoded[['scalar_coupling_constant']])
+	for col in cols_to_encode:
+		df_encoded = df_encoded.join(
+			pd.get_dummies(df[[col]])
+		)
+
+	max_abs_scaler = preprocessing.MaxAbsScaler()
+	df_encoded[['scalar_coupling_constant']] = max_abs_scaler.fit_transform(df_encoded[['scalar_coupling_constant']])
 
 	df_encoded.to_csv(data_out)
 
